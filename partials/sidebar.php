9918e79d453a5bbde08bb52c55544d023317e2a1
anastasyaoctavianti <Hadijahdedeh05@gmail.com>
2023-01-15 01:21:31 +0800

    “Merapikan footer, nav, dan sidebar”

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -24,7 +24,16 @@
           </li>
           <li class="nav-item">
               <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="nav-icon fas fa-copy"></i>
+                  <p>
+                      Master Data
+                      <i class="fas fa-angle-left right"></i>
+                  </p>        
+              </a>
+              </ul class="nav nav-treeview">
+                  <li class="nav-item">
+                      <a href="#" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
                   <p>Lokasi</p>
               </a>
           </li>
