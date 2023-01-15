028dfbea3470924b0526f77554af4b5473672585
anastasyaoctavianti <Hadijahdedeh05@gmail.com>
2023-01-15 23:57:55 +0800

    Menampilkan halaman read lokasi

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ if (isset($_GET['page'])) {
             file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
             break;
         case 'lokasiread';
-        file_exists('pages/admin/lokasihead.php') ? include 'pages/admin/lokasihead.php' : include "pages/404.php";
+        file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "pages/404.php";
         break;
         default:
             include "pages/404.php";
